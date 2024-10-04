9eee953ad743a1fd9ca0548f27d49c669b4f70ac
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-05 01:57:28 +0300

    scroll down the page while preparing

--- src/app/page/loading/primary.py
+++ src/app/page/loading/primary.py
@@ -1,9 +1,13 @@
 import pyautogui as GU
 
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+from app.page.screen.scroll import scroll_down
 from .render import page_render_delay
 
 
-def prepare_page() -> None:
+def prepare_page(driver: WebDriver) -> None:
     GU.hotkey('win', 'up')  # fullscreen
     
     page_render_delay(3)
+    scroll_down(driver, 350)
