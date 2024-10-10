5d42d625d90016e17c32a37cd368f2e36c55c789
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-10 23:06:50 +0300

    delete body from params in `open_new_tab`

--- src/app/page/tabs.py
+++ src/app/page/tabs.py
@@ -1,12 +1,10 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 
-def open_new_tab(url: str, driver: WebDriver, body: WebElement | None=None) -> None:
-    if not body:
-        body = driver.find_element(By.TAG_NAME, 'body')
+def open_new_tab(url: str, driver: WebDriver) -> None:
+    body = driver.find_element(By.TAG_NAME, 'body')
     
     body.send_keys(Keys.CONTROL + 't')  # open new tab
     driver.get(url)
