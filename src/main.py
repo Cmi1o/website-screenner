deeb4c601231ec1e04b429221223b8daceffa306
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-10 23:23:15 +0300

    add dynamic page scrolling

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from app.page import (
 from app.page import cursor
 from app.page.loading import prepare_page
 from app.page.screen.buttons import is_next_page_button_found
+from app.page.screen.sizes import get_screen_size
 from app.page.tabs import open_new_tab
 from screenshots import screens_maker
 from get_url import next_page_url
@@ -34,6 +35,8 @@ def main() -> None:
         cursor.move_to_top()
         
         while has_next_page:
+            screen_height = get_screen_size(driver).height
+            
             while page_screens_count < constants.MAX_SCREENS_COUNT and not is_last_photo:
                 page_screens_count += 1
                 screens_count += 1
@@ -45,7 +48,10 @@ def main() -> None:
                         is_last_photo = True
                         delete_file(screens_count)
                 
-                scroll_down(driver)
+                scroll_down(
+                    driver=driver,
+                    step=screen_height * constants.PAGE_SCROLL_PERCENT
+                )
                 page_render_delay(1.5)
             
             if is_next_page_button_found(driver):
