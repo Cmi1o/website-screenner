e5ddfe6dd485da9abb356a0dee049059f2376cb5
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-21 01:05:06 +0300

    change screenshots path constant: add new `{}` to add page url hash

--- src/constants/wb_page.py
+++ src/constants/wb_page.py
@@ -1,4 +1,4 @@
 MAX_SCREENS_COUNT: int = 25
 NEXT_PAGE_ENDPOINT: str = '?sort=popular&page='
 PAGE_SCROLL_PERCENT: float = 0.5852
-SCREENSHOT_PATH: str = 'assets/screenshot_{}.png'
+SCREENSHOT_PATH: str = 'assets/screenshot_{}_{}.png'
